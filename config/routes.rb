ce61573745a681ecffb460a52171a2a0f33a6e94
vincent jonathan <v.jonathan94@gmail.com>
2017-04-28 14:08:47 +0700

    create 4 controller represented 4 static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'contact/index'
+
+  get 'about/index'
+
+  get 'welcome/index'
+
+  get 'services/index'
+
+  root 'welcome#index'
+end  
